0fa5b46218bf717c11eea0fc85ac437807327499
Jalaj Minda <jaminda@microsoft.com>
2019-11-01 15:00:57 +0530

    PR review comments

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core';
-import * as fs from 'fs';
+import * as exec from '@actions/exec';
 import * as io from '@actions/io';
-import { getScriptFileName, giveExecutablePermissionsToFile, executeScript, tempDirectory, ExecuteScriptModel, FileNameModel } from './utils';
+import * as os from 'os';
 
-const bashArg = 'bash --noprofile --norc -eo pipefail';
+import { createScriptFile, TEMP_DIRECTORY, START_SCRIPT_EXECUTION, NullOutstreamStringWritable } from './utils';
+
+const BASH_ARG: string = `bash --noprofile --norc -eo pipefail -c "echo '${START_SCRIPT_EXECUTION}' >&2;`;
+const CONTAINER_WORKSPACE: string = '/github/workspace';
+const CONTAINER_TEMP_DIRECTORY: string = '/_temp';
 
 const run = async () => {
 
@@ -12,21 +16,22 @@ const run = async () => {
             core.setFailed('Please use Linux based OS as a runner.');
             return;
         }
-        let inlineScript: string = core.getInput('inlineScript');
-        let azcliversion: string = core.getInput('azcliversion').trim();
 
-        if (!(await checkIfValidVersion(azcliversion))) {
-            core.setFailed('Please enter a valid azure cli version. \nRead more about Azure CLI versions: https://github.com/Azure/azure-cli/releases.');
+        let inlineScript: string = core.getInput('inlineScript', { required: true });
+        let azcliversion: string = core.getInput('azcliversion', { required: true }).trim();
+
+        if (!(await checkIfValidCLIVersion(azcliversion))) {
+            core.setFailed('Please enter a valid azure cli version. \nSee available versions: https://github.com/Azure/azure-cli/releases.');
             return;
         }
+
         if (!inlineScript.trim()) {
             core.setFailed('Please enter a valid script.');
             return;
         }
-        const { fileName, fullPath } = <FileNameModel>getScriptFileName();
-        fs.writeFileSync(fullPath, `${inlineScript}`);
-        await giveExecutablePermissionsToFile(fullPath);
-        let bashCommand: string = ` ${bashArg} /_temp/${fileName} `;
+        const scriptFile: string = await createScriptFile(inlineScript);
+        let bashCommand: string = ` ${BASH_ARG}${CONTAINER_TEMP_DIRECTORY}/${scriptFile} `;
+
         /*
         For the docker run command, we are doing the following
         - Set the working directory for docker continer
@@ -34,46 +39,75 @@ const run = async () => {
         - voulme mount .azure session token file between host and container,
         - volume mount temp directory between host and container, inline script file is created in temp directory
         */
-        let command: string = `run --workdir /github/workspace -v ${process.env.GITHUB_WORKSPACE}:/github/workspace `;
-        command += ` -v /home/runner/.azure:/root/.azure -v ${tempDirectory}:/_temp `;
+        let command: string = `run --workdir ${CONTAINER_WORKSPACE} -v ${process.env.GITHUB_WORKSPACE}:${CONTAINER_WORKSPACE} `;
+        command += ` -v ${process.env.HOME}/.azure:/root/.azure -v ${TEMP_DIRECTORY}:${CONTAINER_TEMP_DIRECTORY} `;
+        command += `-e GITHUB_WORKSPACE=${CONTAINER_WORKSPACE}`;
         command += ` mcr.microsoft.com/azure-cli:${azcliversion} ${bashCommand}`;
+
         await executeDockerScript(command);
         console.log("az script ran successfully.");
     } catch (error) {
-        console.log("az CLI action failed.\n\n", error);
+        console.log("Azure CLI action failed.\n\n", error);
         core.setFailed(error.stderr);
     }
 };
 
-const checkIfValidVersion = async (azcliversion: string): Promise<boolean> => {
+const checkIfValidCLIVersion = async (azcliversion: string): Promise<boolean> => {
     const allVersions: Array<string> = await getAllAzCliVersions();
-    for (let i: number = allVersions.length - 1; i >= 0; i--) {
-        if (allVersions[i].trim() === azcliversion) {
-            return true;
-        }
+    if (!allVersions) {
+        return true;
     }
-    return false;
+    return allVersions.some((eachVersion) => eachVersion === azcliversion);
 }
 
 const getAllAzCliVersions = async (): Promise<Array<string>> => {
+    var outStream: string = '';
+    var execOptions: any = {
+        outStream: new NullOutstreamStringWritable({ decodeStrings: false }),
+        listeners: {
+            stdout: (data: any) => outStream += data.toString() + os.EOL, //outstream contains the list of all the az cli versions
+        }
+    };
 
-    const { outStream, errorStream, errorCaught } = <ExecuteScriptModel>await executeScript(`curl --location -s https://mcr.microsoft.com/v2/azure-cli/tags/list`);
     try {
+        await exec.exec(`curl --location -s https://mcr.microsoft.com/v2/azure-cli/tags/list`, [], execOptions)
         if (outStream && JSON.parse(outStream).tags) {
             return JSON.parse(outStream).tags;
         }
     } catch (error) {
-        throw new Error(`Unable to fetch all az cli versions, please report it as a issue. outputstream contains ${outStream}, error = ${errorStream}\n${errorCaught}`);
+        // if output is 404 page not found, please verify the url
+        throw new Error(`Unable to fetch all az cli versions, please report it as an issue on https://github.com/Azure/CLI/issues. Output: ${outStream}, Error: ${error}`);
     }
     return [];
 }
 
 const executeDockerScript = async (dockerCommand: string): Promise<void> => {
-    const dockerPath: string = await io.which("docker", true);
-    const { outStream, errorStream, errorCaught } = <ExecuteScriptModel>await executeScript(dockerCommand, dockerPath);
-    console.log(outStream);
-    if (errorCaught) {
-        throw new Error(`az CLI script failed, Please check the script.\nPlease refer the script error at the end after docker logs.\n\nDocker logs...\n${errorStream}.`);
+
+    const dockerTool: string = await io.which("docker", true);
+    var errorStream: string = '';
+    var execOptions: any = {
+        outStream: new NullOutstreamStringWritable({ decodeStrings: false }),
+        listeners: {
+            stdout: (data: any) => console.log(data.toString()), //to log the script output while the script is running.
+            errline: (data: any) => {
+                if (data.toString().trim() === START_SCRIPT_EXECUTION) {
+                    errorStream = ''; // Flush the container logs. After this, script error logs will be tracked.
+                }
+                else {
+                    errorStream += data.toString() + os.EOL;
+                }
+            }
+        }
+    };
+
+    try {
+        await exec.exec(`"${dockerTool}" ${dockerCommand}`, [], execOptions)
+    } catch (error) {
+        if (errorStream) {
+            throw new Error(errorStream);
+        } else {
+            throw error;
+        }
     }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -2,82 +2,35 @@ import stream = require('stream');
 import * as exec from '@actions/exec';
 import * as path from 'path';
 import * as os from 'os';
+import * as fs from 'fs';
 
-export const tempDirectory: string = process.env.RUNNER_TEMP || os.tmpdir();
+export const TEMP_DIRECTORY: string = process.env.RUNNER_TEMP || os.tmpdir();
+export const START_SCRIPT_EXECUTION: string = 'Azure CLI GitHub Action: Starting script execution';
 
-export interface ExecuteScriptModel {
-    outStream: string;
-    errorStream: string;
-    errorCaught: any;
-};
-
-export interface FileNameModel {
-    fileName: string;
-    fullPath: string;
-};
-
-export const giveExecutablePermissionsToFile = async (filePath: string): Promise<void> => await executeCommand(`chmod +x ${filePath}`, { silent: true })
-
-export const getScriptFileName = (): FileNameModel => {
+export const createScriptFile = async (inlineScript: string): Promise<string> => {
     const fileName: string = `AZ_CLI_GITHUB_ACTION_${getCurrentTime().toString()}.sh`;
-    const fullPath: string = path.join(tempDirectory, fileName);
-    return { fileName, fullPath };
+    const filePath: string = path.join(TEMP_DIRECTORY, fileName);
+    fs.writeFileSync(filePath, `${inlineScript}`);
+    await giveExecutablePermissionsToFile(filePath);
+    return fileName;
 }
+
+export const giveExecutablePermissionsToFile = async (filePath: string): Promise<number> => await exec.exec(`chmod +x ${filePath}`, [], { silent: true })
 
 export const getCurrentTime = (): number => {
     return new Date().getTime();
 }
 
-export const executeCommand = async (command: string, execOptions = {}, toolPath?: string): Promise<void> => {
-    try {
-        if (toolPath) {
-            command = `"${toolPath}" ${command}`;
-        }
-        await exec.exec(command, [], execOptions);
-    }
-    catch (error) {
-        throw new Error(error);
-    }
-}
-
-export const executeScript = async (command: string, toolPath: string = ''): Promise<ExecuteScriptModel> => {
-    var outStream: string = '';
-    var errorStream: string = '';
-    var errorCaught: string = '';
-    try {
-        await executeCommand(command, {
-            outStream: new StringWritable({ decodeStrings: false }),
-            errStream: new StringWritable({ decodeStrings: false }),
-            listeners: {
-                stdout: (data: Buffer) => outStream += data.toString(),
-                stderr: (data: Buffer) => errorStream += data.toString()
-            }
-        }, toolPath);
-    }
-    catch (error) {
-        errorCaught = error;
-    }
-    finally {
-        return { outStream, errorStream, errorCaught };
-    }
-}
-
-class StringWritable extends stream.Writable {
-    private value: string = '';
+export class NullOutstreamStringWritable extends stream.Writable {
 
     constructor(options: any) {
         super(options);
     }
 
     _write(data: any, encoding: string, callback: Function): void {
-
-        this.value += data.toString();
         if (callback) {
             callback();
         }
     }
-
-    toString(): string {
-        return this.value;
-    }
 };
+
