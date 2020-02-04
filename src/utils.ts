f089509e3cd4bfd941904c557f3e16b56757de6f
Jalaj Minda <49639049+jalajmsft@users.noreply.github.com>
2020-02-04 11:28:04 +0530

    Allowing user-defined env variable to be accessible (#16)
    * Allowing user-defined env variable to be accessible
    
    * Resolving PR comments

--- src/utils.ts
+++ src/utils.ts
@@ -45,3 +45,52 @@ export class NullOutstreamStringWritable extends stream.Writable {
         }
     }
 };
+
+export const checkIfEnvironmentVariableIsOmitted = (key: string): boolean => {
+
+    const omitEnvironmentVariables: string [] = [
+        'LANG',
+        'HOSTNAME',
+        'PWD',
+        'HOME',
+        'PYTHON_VERSION',
+        'PYTHON_PIP_VERSION',
+        'SHLVL',
+        'PATH',
+        'GPG_KEY',
+        'CONDA',
+        'AGENT_TOOLSDIRECTORY',
+        'GITHUB_WORKSPACE',
+        'RUNNER_PERFLOG',
+        'RUNNER_WORKSPACE',
+        'RUNNER_TEMP',
+        'RUNNER_TRACKING_ID',
+        'RUNNER_TOOL_CACHE',
+        'DOTNET_SKIP_FIRST_TIME_EXPERIENCE',
+        'JOURNAL_STREAM',
+        'DEPLOYMENT_BASEPATH',
+        'VCPKG_INSTALLATION_ROOT',
+        'PERFLOG_LOCATION_SETTING'
+    ];
+
+    const omitEnvironmentVariablesWithPrefix: string [] = [
+        'JAVA_',
+        'LEIN_',
+        'M2_',
+        'BOOST_',
+        'GOROOT',
+        'ANDROID_',
+        'GRADLE_',
+        'ANT_',
+        'CHROME',
+        'SELENIUM_',
+        'INPUT_'
+    ];
+    for (let i = 0; i < omitEnvironmentVariables.length; i++){
+        if (omitEnvironmentVariables[i] === key.toUpperCase()){
+            return true;
+        }
+    }
+
+    return omitEnvironmentVariablesWithPrefix.some((prefix: string) => key.toUpperCase().startsWith(prefix));
+}
