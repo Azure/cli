ae812da3ea9f6248fa9884330fb89b2c28e4ec72
milidoshi26 <miliigcse@gmail.com>
2021-07-15 10:15:30 +0530

    revert

---
+++ node_modules/credscan-pkg/src/ExternalLibrary/Libarary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExternalLibrary
+{
+    public class Person
+    {
+        public Guid Id => Guid.NewGuid();
+        public string Name => "John Smith";
+        public string Email => "john.smith@electron-quick-start.com";
+
+    }
+
+    public class Library
+    {
+        public Person GetPerson()
+        {
+            return new Person();
+        }
+    }
+}
---
+++ node_modules/credscan-pkg/src/QuickStart.Standard/ExternalLibarary.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using ExternlaLibrary.Standard;
+using Newtonsoft.Json;
+
+namespace QuickStart.Standard
+{
+    class ExternalMethods
+    {
+        private readonly Library _library = new Library();
+
+        public async Task<object> GetPersonInfo(dynamic input)
+        {
+            return await Task.Run(() =>JsonConvert.SerializeObject(_library.GetPerson(), Formatting.Indented));
+        }
+    }
+}
